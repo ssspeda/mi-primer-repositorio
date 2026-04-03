559401e5ac084b101a39f23ffa4c2be60207d5db
ssspeda <matematecap@gmail.com>
2026-04-03 20:25:49 -0300

    tp1 terminado

---
+++ tp1/ej7.py
@@ -0,0 +1,45 @@
+"""Slicing."""
+
+
+def es_palindromo(palabra: str) -> bool:
+    """Toma un string y devuelve un booleano en base a si se lee igual al
+    derecho y al revés.
+
+    Restricción: No utilizar bucles - Usar Slices de listas.
+    Referencia: https://docs.python.org/3/tutorial/introduction.html#lists
+    """
+    pass  # Completar
+
+    palabra_limpia = palabra.replace(" ", "").lower()
+    return palabra_limpia == palabra_limpia[::-1]
+
+
+# NO MODIFICAR - INICIO
+assert not es_palindromo("amor")
+assert es_palindromo("radar")
+assert es_palindromo("")
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def mitad(palabra: str) -> str:
+    """Toma un string y devuelve la mitad. Si la longitud es impar, redondear
+    hacia arriba.
+
+    Restricción: No utilizar bucles - Usar Slices de listas.
+    Referencia: https://docs.python.org/3/tutorial/introduction.html#lists
+    """
+    pass  # Completar
+
+    longitud = len(palabra)
+    mitad = (longitud + 1) // 2
+    return palabra[:mitad]
+
+
+# NO MODIFICAR - INICIO
+assert mitad("hello") == "hel"
+assert mitad("Moon") == "Mo"
+assert mitad("") == ""
+# NO MODIFICAR - FIN
---
+++ tp1/ej9.py
@@ -0,0 +1,77 @@
+"""FOR, Sum, Reduce."""
+
+
+from functools import reduce
+
+
+def sumatoria_basico(n: int) -> int:
+    """Devuelve la suma de los números de 1 a N.
+
+    Restricción: Utilizar un bucle for.
+    """
+    pass  # Completar
+
+    total = 0
+    i = 1
+    while i <= n:
+        total = total + i
+        i = i + 1
+    return total
+
+
+# NO MODIFICAR - INICIO
+assert sumatoria_basico(1) == 1
+assert sumatoria_basico(100) == 5050
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def sumatoria_sum(n: int) -> int:
+    """Re-Escribir utilizando la función sum y sin usar bucles.
+    Referencia: https://docs.python.org/3/library/functions.html#sum
+    """
+    pass  # Completar
+
+    return sum(range(1, n + 1))
+
+
+# NO MODIFICAR - INICIO
+assert sumatoria_sum(1) == 1
+assert sumatoria_sum(100) == 5050
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def sumatoria_reduce(n: int) -> int:
+    """CHALLENGE OPCIONAL: Re-escribir utilizando reduce.
+    Referencia: https://docs.python.org/3/library/functools.html#functools.reduce
+    """
+    pass  # Completar
+
+
+# NO MODIFICAR - INICIO
+if __name__ == "__main__":
+    assert sumatoria_reduce(1) == 1
+    assert sumatoria_reduce(100) == 5050
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def sumatoria_gauss(n: int) -> int:
+    """CHALLENGE OPCIONAL: Re-Escribir utilizando suma de Gauss.
+    Referencia: https://es.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
+    """
+    pass  # Completar
+
+
+# NO MODIFICAR - INICIO
+if __name__ == "__main__":
+    assert sumatoria_gauss(1) == 1
+    assert sumatoria_gauss(100) == 5050
+# NO MODIFICAR - FIN
